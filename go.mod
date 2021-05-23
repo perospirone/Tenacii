a6a5b264edf48d001c2bbd7fc084614caeafb505
danitw <danidevpy@gmail.com>
2021-05-22 22:13:22 -0300

    hello world fyne

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tenacii
 
 go 1.15
+
+require fyne.io/fyne/v2 v2.0.3
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("hello tenacii")
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
 }
